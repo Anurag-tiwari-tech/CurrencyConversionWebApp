67fde6bef646019a8cf7266ecac05021843ce223
Anurag-tiwari-tech <68097159+Anurag-tiwari-tech@users.noreply.github.com>
2021-03-12 17:00:23 +0530

    Add files via upload

---
+++ src/FormSuccess.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Form.css';
+import Main from './Homepage.js';
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+      <Main />
+    </div>
+  );
+};
+
+export default FormSuccess;
